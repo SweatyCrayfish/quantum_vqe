4739a2b5621913f0080f16e69cdcafdd9667b780
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-03 18:15:21 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantum_vqe",
-    version="0.1.0",
+    version="0.1.2.1",
     packages=find_packages(),
     install_requires=["qiskit", "numpy", "scipy"],
     author="Viktor Veselov",
